7606f017e103a4c5803933597ddc7691ac63487b
anoosm <anoosalathur@gmail.com>
2023-09-20 17:30:09 +0530

    implemnted the CalculatorActions sealed class to define the actions the calculator can perform

--- app/src/main/java/com/example/composecalculatorapp/CalculatorActions.kt
+++ app/src/main/java/com/example/composecalculatorapp/CalculatorActions.kt
@@ -1,3 +1,11 @@
 package com.example.composecalculatorapp
 
-sealed class CalculatorActions
+sealed class CalculatorActions {
+    data class Number(val number: Int): CalculatorActions()
+    object Clear: CalculatorActions()
+    object Delete: CalculatorActions()
+    object Decimal: CalculatorActions()
+    object Calculate: CalculatorActions()
+    data class Operations(val operations: CalculatorOperations): CalculatorActions()
+
+}
